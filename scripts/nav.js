3c07359302ae5ddefeeb9d70444e19ced158c459
tiago-neto1027 <145197457+tiago-neto1027@users.noreply.github.com>
2023-11-03 15:48:52 +0000

    Remodelacao Navbar
    A Navbar foi remodelada, embora esteja idêntica à antiga, agora usa o Bootstrap para a sua criação.

--- scripts/nav.js
+++
@@ -1,9 +0,0 @@
-/* Botão da Nav */
-/* Mostra e Esconde o Menu */
-document.getElementById("navBtn").onclick = function() {
-    abrirNav()
-};
-
-function abrirNav() {
-    document.getElementById("abrirMenu").classList.toggle("navAberta");
-}
